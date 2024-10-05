ecb427afdabe0a36d815a2cf28e6efb1ff4c4554
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2024-10-06 00:51:53 +0800

    added material

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LandingPage from './pages/LandingPage'; // Import your additional page
+import Phytoplankton from './pages/Phytoplankton';
+import AerosolData from './pages/AerosolData';
+import CloudProperties from './pages/CloudProperties';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/phytoplankton" element={<Phytoplankton />} />
+        <Route path="/aerosoldata" element={<AerosolData />} />
+        <Route path="/cloudproperties" element={<CloudProperties />} />
       </Routes>
     </Router>
   );
