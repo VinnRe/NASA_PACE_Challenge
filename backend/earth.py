5cdcbf456610b7aef8edbd00d4c355d74db38401
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-10-05 21:45:14 +0800

    Add data retriever

---
+++ backend/earth.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+import earthaccess
+
+load_dotenv()
+
+earthaccess.login(strategy="environment")
+
+app = Flask(__name__)
+
+def authenticate():
+    try:
+        session = earthaccess.login(strategy="environment")
+        return session
+    except Exception as e:
+        return str(e)
+
+@app.route('/api/phytoplankton-data', methods=['GET'])
+def get_phytoplankton_data():
+    try:
+        session = authenticate()
+        if isinstance(session, str):
+            return jsonify({"error": "Authentication Failed: " + session}), 500
+
+        datasets = earthaccess.search_datasets(
+            keyword="clouds",
+            cloud_hosted=True
+        )
+
+        dataset_list = []
+        for dataset in datasets[0:10]: # REMOVE THE "[0:10]" TO GET ALL THE DATA
+            dataset_list.append(dataset)
+
+        return jsonify(dataset_list)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
