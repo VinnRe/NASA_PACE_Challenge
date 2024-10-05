8bdf8254ba8202bad5785cd37bdc936f0b076030
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-10-05 11:01:42 +0800

    Initial Commit

--- backend/main.py
+++ backend/main.py
@@ -23,8 +23,6 @@ def get_pace_data():
             return jsonify({"error": "Authentication Failed: " + session}), 500
         
         results = earthaccess.search_data(short_name='CLDMSK_L2_VIIRS_SNPP',
-                                            # bounding_box=(-10, 20, 10, 50),
-                                            # temporal=("2023-03", "2023-03"),
                                             count=1, 
                                             cloud_hosted=True)
 
